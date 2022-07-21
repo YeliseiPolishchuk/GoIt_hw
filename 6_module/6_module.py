ed0004f76550a43c3095daffb5f16c3c6ebda885
Yelisei Polishchuk <yelisei.polishchuk@gmail.com>
2022-07-21 14:12:47 +0300

    working

--- 6_module/6_module.py
+++ 6_module/6_module.py
@@ -1,1 +1,56 @@
 import sys
+from pathlib import Path
+from shutil import copyfile
+
+# расширения файлов для сортровки
+photo_ext = ['.jpeg', '.png', '.jpg', '.svg']
+vid_ext = ['.avi', '.mp4', '.mov', '.mkv']
+doc_ext = ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx']
+mus_ext = ['.mp3', '.ogg', '.wav', '.amr']
+arch_ext = ['.zip', '.gz', '.tar']
+
+folder_path = Path(sys.argv[1])
+
+sorted_folder_path = Path('./6_module/sorted')  # Создаю папку для отсортированных файлов
+sorted_folder_path.mkdir(exist_ok=True, parents=True)
+
+
+def read_folder(path: Path) -> None:
+    '''Функция прохода по файлам в папке'''
+    for elem in path.iterdir():
+        if elem.is_dir():
+            read_folder(elem)
+        else:
+            copy_file(elem)
+
+
+def copy_file(file: Path) -> None:
+    '''Функция, которая копирует файл, изменяет имя и отправляет в отсортированную папку'''
+    ext = file.suffix  # расширение файла
+    if ext in photo_ext:
+        new_path = sorted_folder_path/'photos'
+        new_path.mkdir(exist_ok=True)
+        copyfile(file, new_path/file.name)
+    elif ext in vid_ext:
+        new_path = sorted_folder_path/'videos'
+        new_path.mkdir(exist_ok=True)
+        copyfile(file, new_path/file.name)
+    elif ext in doc_ext:
+        new_path = sorted_folder_path/'docs'
+        new_path.mkdir(exist_ok=True)
+        copyfile(file, new_path/file.name)
+    elif ext in mus_ext:
+        new_path = sorted_folder_path/'music'
+        new_path.mkdir(exist_ok=True)
+        copyfile(file, new_path/file.name)
+    elif ext in arch_ext:
+        new_path = sorted_folder_path/'archives'
+        new_path.mkdir(exist_ok=True)
+        copyfile(file, new_path/file.name)
+    else:
+        new_path = sorted_folder_path/'else'
+        new_path.mkdir(exist_ok=True)
+        copyfile(file, new_path/file.name)
+
+
+read_folder(folder_path)
--- Модуль 6.1/test2.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-p = Path('abc')
-p.mkdir(exist_ok=True)  # Если директория существует, то не вызовет ошибку
-p.rmdir()  # Удалить папку
-
-p = Path('abc/temp')
-p.mkdir(exist_ok=True, parents=True)  # создать папку внутри папки
